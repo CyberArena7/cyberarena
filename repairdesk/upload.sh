ee0cd137ea16fe8f36c7c231035616bda7f29fe6
CyberArena7 <github@cyberarena.es>
2025-09-19 17:51:00 +0200

    Update upload.sh

--- repairdesk/upload.sh
+++ repairdesk/upload.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 set -eu
 
+# Movernos a la carpeta del script (repairdesk/)
 cd "$(dirname "$0")"
-uv build
 
 SCP=${SCP:-scp}
 SSH=${SSH:-ssh}
 
+# Construir el wheel (sin -C)
 uv build
+
 cd dist
 NAME=$(echo repairdesk-*-py3-none-any.whl)
 
-"$SCP" "$NAME" "$1:/tmp"
-"$SSH" "$1" "sudo pip3 install --no-deps --break-system-packages --force-reinstall /tmp/$NAME"
+$SCP "$NAME" "$1:/tmp"
+$SSH "$1" "sudo pip3 install --no-deps --break-system-packages --force-reinstall /tmp/$NAME"
